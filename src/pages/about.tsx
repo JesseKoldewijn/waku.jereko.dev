1d5e079c745d5ec685d9f7c524c70fd49184120f
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-26 11:31:43 +0200

    init: core setup

---
+++ src/pages/about.tsx
@@ -0,0 +1,35 @@
+import { Link } from "waku";
+
+const AboutPage = async () => {
+	const data = await getData();
+
+	return (
+		<div>
+			<title>{data.title}</title>
+			<h1 className="text-4xl font-bold tracking-tight">
+				{data.headline}
+			</h1>
+			<p>{data.body}</p>
+			<Link to="/" className="mt-4 inline-block underline">
+				Return home
+			</Link>
+		</div>
+	);
+};
+export default AboutPage;
+
+const getData = async () => {
+	const data = {
+		title: "About",
+		headline: "About Waku",
+		body: "The minimal React framework",
+	};
+
+	return data;
+};
+
+export const getConfig = async () => {
+	return {
+		render: "static",
+	};
+};
