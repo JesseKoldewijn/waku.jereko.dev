4bd65e90b2f4daabd3c4e02b665adb4e8ae7f7c3
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-05-01 14:14:34 +0200

    fix: added default headers go api in serverless function

--- api/index.go
+++ api/index.go
@@ -11,6 +11,17 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	// Create router
 	router := gin.Default()
 
+	// Default headers
+	router.Use(func(c *gin.Context) {
+		// origin of same domain is allowed 
+		c.Header("Access-Control-Allow-Origin", "*") 
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		c.Header("Content-Type", "application/json")
+		c.Header("Accept", "application/json")
+		c.Next()
+	})
+
 	// Routes
 	routes.RouteGroupRoot(router)
 
